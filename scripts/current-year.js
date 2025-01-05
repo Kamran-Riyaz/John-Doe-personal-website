bcf5b3c59e37fecd524f1018f7208754ccd9ddf2
Kamran Riyaz <kamranriyazw@gmail.com>
2025-01-05 21:14:30 +0530

    Add fictional content

---
+++ scripts/current-year.js
@@ -0,0 +1,4 @@
+// Get the current year dynamically
+const currentYear = new Date().getFullYear();
+// Set the year in the span with ID 'year'
+document.getElementById('year').textContent = currentYear;
